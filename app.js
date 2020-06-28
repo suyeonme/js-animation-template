1cef9d6b89e99d39dd24cd63b0bce19ad23b9a3a
Suyeon Kang <tndusrkd92@gmail.com>
2020-06-28 12:32:29 +0900

    Mouse cursor animation

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict'
 
-/////////////////////
+/////////////////////////////
 // IMAGE EXPAND MODAL
-const imageModal = () => {
+const imageModal = (function() {
     const modal = document.querySelector('.modal');
     const previews = Array.from(document.querySelectorAll('.img-container img'));
     const modalImage = document.querySelector('.full-img');
@@ -14,23 +14,51 @@ const imageModal = () => {
             modal.classList.add('open');
             modalImage.classList.add('open')
 
-            // Change text
-            const modalImageSrc = preview.getAttribute('data-original');
-            modalImage.src = `./large/${modalImageSrc}`;
-
             // Change image
+            const modalImageSrc = preview.getAttribute('data-original');
+            modalImage.src = `./img/large/${modalImageSrc}`;
+
+            // Change text
             const altText = preview.alt;
             modalText.textContent = altText;
         });
     });
 
     // Close modal 
-    modal.addEventListener('click' , e => {
-        if (e.target.classList.contains('open')) {
-            modal.classList.remove('open');
-            modalImage.classList.remove('open');
-        };
-    });
-};
+    if (modal) {
+        modal.addEventListener('click' , e => {
+            if (e.target.classList.contains('open')) {
+                modal.classList.remove('open');
+                modalImage.classList.remove('open');
+            };
+        });
+    }
+})();
 
-imageModal();
+
+/////////////////////////////
+// MOUSE CURSOR 
+const mouseCursor = (function(){
+    const mouseCursor = document.querySelector('.cursor');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    const cursor = e => {
+        mouseCursor.style.top = e.pageY + 'px';
+        mouseCursor.style.left = e.pageX + 'px';
+    };
+
+    window.addEventListener('mousemove', cursor);
+
+    navLinks.forEach(link => {
+        link.addEventListener('mouseleave', () => {
+            mouseCursor.classList.remove('link-grow');
+            link.classList.remove('hovered-link');
+        });
+        
+        link.addEventListener('mouseover', () => {
+            mouseCursor.classList.add('link-grow');
+            link.classList.add('hovered-link');
+        });
+    });
+})();
+
