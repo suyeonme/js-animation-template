d28b0615d19c877c39c588df4a6ff466065d03ef
Suyeon Kang <tndusrkd92@gmail.com>
2020-07-03 17:08:57 +0900

    Page animation with GSAP

--- js/app.js
+++ js/app.js
@@ -215,9 +215,9 @@
 
 
 /////////////////////////////
-// FULL PAGE ANIMATION
+// FULL PAGE ANIMATION 
 // fullPage, tweenMax, and timeline max
-const fullPage = (function() {
+/* const fullPage = (function() {
 
     new fullpage('#full-page', {
         autoScrolling: true,
@@ -233,13 +233,34 @@ const fullPage = (function() {
                 const chairs = document.querySelectorAll('.chair');
                 const des = document.querySelector('.description');
 
-                timeLine.fromTo(chairs, 0.7, { x: '100%' }, { x: '-35%' })
-                    .fromTo(des, 0.5, { y: 50, opacity: 0 }, { y: 0, opacity: 1})
-                    .fromTo(chairs[0], 1, { opacity: 1 }, { opacity: 1 })
+                timeLine
+                    .fromTo(chairs, 0.5, { x: '100%' }, { x: '-35%' })
+                    .fromTo(des, 0.7, { y: 50, opacity: 0 }, { y: 0, opacity: 1})
+                    .fromTo(chairs[0], 0.7, { opacity: 0 }, { opacity: 1 })
                     .fromTo(chairs[1], 1, { opacity: 0 }, { opacity: 1 })
                     .fromTo(chairs[2], 1, { opacity: 0 }, { opacity: 1 })
             };
         } 
     });
 
+})(); */
+
+
+/////////////////////////////
+// PAGE ANIMATION WITH GSAP
+const pageAnimation = (function(){
+    const hero = document.querySelector('.hero');
+    const slider = document.querySelector('.slider');
+    const logo = document.querySelector('#logo');
+    const menu = document.querySelector('.menu');
+    const headline = document.querySelector('.headline');
+
+    const timeline = new TimelineMax();
+    timeline
+        .fromTo(hero, 1, { height: '0%' }, { height: '80%', ease: Power2.easeInOut })
+        .fromTo(hero, 1.2, { width: '100%' }, { width: '80%', ease: Power2.easeInOut })
+        .fromTo(slider, 1.2, { x: '-100%' }, { x: '0%', ease: Power2.easeInOut }, "-=1.2") // overlap or gaps ("+=1")
+        .fromTo(logo, 0.5, { opacity: '0', x: '30' }, { opacity: '1', x: '0', ease: Power2.easeInOut }, "-=0.5")    
+        .fromTo(menu, 0.5, { opacity: '0', x: '30' }, { opacity: '1', x: '0', ease: Power2.easeInOut }, "-=0.5")  
+        .fromTo(headline, 0.5, { opacity: '0', x: '30' }, { opacity: '1', x: '0', ease: Power2.easeInOut }, "-=0.5")     
 })();
