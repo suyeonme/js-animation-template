89db690f408b5f01a9e94bb6c726d4d01fcfbeb5
Suyeon Kang <tndusrkd92@gmail.com>
2020-07-27 19:25:51 +0900

    3D cubic image slider with swiper js

--- js/app.js
+++ js/app.js
@@ -719,7 +719,7 @@ scrollTrigger(); */
 
 ///////////////////////////////////////////
 //  SVG TEXT PATH ANIMATION
-const textPathAnim = () => {
+/* const textPathAnim = () => {
     const textPath = document.querySelector('#text-path');
     const titles = document.querySelectorAll('.anim');
     
@@ -753,4 +753,23 @@ const textPathAnim = () => {
 };
 
 textPathAnim();
+ */
 
+
+
+///////////////////////////////////////////
+//  3D CUBE EFFECT WITH SWIPER JS
+var swiper = new Swiper('.swiper-container', {
+    effect: 'cube',
+    grabCursor: true,
+    cubeEffect: {
+        shadow: false,
+        slideShadows: false,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
